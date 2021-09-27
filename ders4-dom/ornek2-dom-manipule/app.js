799278e50e4cb6a549b1d6a8e260849759285a41
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-09-27 23:01:09 +0200

    tahmin oyunu

--- ders4-dom/ornek2-dom-manipule/app.js
+++ ders4-dom/ornek2-dom-manipule/app.js
@@ -33,5 +33,47 @@ document.querySelector(".ekle").onclick = function() {
 
 document.querySelector(".sil").onclick = function() {
     const liste = document.querySelector(".liste");
-    liste.removeChild(liste.lastChild);
+    liste.removeChild(liste.lastElementChild);
+};
+
+//todo  programlama dillerine h1 create ediyoruz..
+
+const paragraf = document.querySelector(".forH1");
+paragraf.innerHTML = `<h1>${"Programlama Dilleri"}</h1>`;
+
+//! Klavyeden bir tuşa basıldığında büyüsün-küçülsün
+
+document.querySelector(".textbox").onkeyup = function() {
+    const checkbox = document.querySelector(".checkbox");
+    const textbox = document.querySelector(".textbox");
+
+    if (checkbox.checked) {
+        textbox.value = textbox.value.toUpperCase();
+    } else {
+        textbox.value = textbox.value.toLoverCase();
+    }
+};
+
+//! mouse  resmin üzerine geldiğinde aslan kükresin
+
+resim.onmouseover = function() {
+    resim.src = "./img/aslan2.jpeg";
+};
+
+//todo mouse üzerinden çekildiğinde
+resim.onmouseout = function() {
+    resim.src = "./img/aslan1.jpeg";
+};
+
+//! Delete ve Enter tuşu ile ekleme ve çıkarma
+
+document.querySelector(".dil").onkeydown = function(klavye) {
+    if (klavye.keyCode == 13) {
+        //!Enter ı çağırma
+        document.querySelector(".ekle").onclick();
+    }
+    if (klavye.keyCode == 46) {
+        //todo Delete yi çağırma
+        document.querySelector(".sil").onclick();
+    }
 };

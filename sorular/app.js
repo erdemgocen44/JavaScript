ea562a5420120d4e949d06d1e89b1da5ecf388e6
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-08 19:25:34 +0200

    salihHoca

---
+++ sorular/app.js
@@ -0,0 +1,16 @@
+function myFunction1() {
+  if (document.getElementById("firstQuestion").hidden == true) {
+    document.getElementById("firstQuestion").hidden.false;
+  } else document.getElementById("firstQuestion").hidden = true;
+}
+
+function myFunction2() {
+  if (document.getElementById("secondQuestion").hidden == true) {
+    document.getElementById("secondQuestion").hidden.false;
+  } else document.getElementById("secondQuestion").hidden = true;
+}
+function myFunction3() {
+  if (document.getElementById("thirdQuestion").hidden == true) {
+    document.getElementById("thirdQuestion").hidden.false;
+  } else document.getElementById("thirdQuestion").hidden = true;
+}
